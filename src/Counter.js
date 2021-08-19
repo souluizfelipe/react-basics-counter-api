2f73daf67962d02f250898675fa754a1eb690ad0
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-19 10:50:05 -0300

    first commit, create Counter component and Counter css and removed unused files

---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import  React, { useState } from 'react';
+
+import './Counter.css';
+
+function Counter() {
+
+  const [ counter, setCounter ] = useState(0);
+
+  const handleOnClick = operator => {
+    const newValue = operator === '+' 
+      ? counter + 1 
+      : counter - 1
+    
+    setCounter(newValue);
+  };
+
+  return(
+    <div className="counter">
+      <span>{counter}</span>
+      <button onClick={() => handleOnClick('-')} >-</button>
+      <button onClick={() => handleOnClick('+')} >+</button>
+    </div>
+  );
+
+};
+
+export default Counter;
